105a52769facd85e9a1b7b289256eda89700fdae
Henrik Malmer Lassen <lasssenn@gmail.com>
2018-05-25 09:13:51 +0200

    Major rewamp of sql & bug fix

--- Arkiv/Data/ISqlData.cs
+++ Arkiv/Data/ISqlData.cs
@@ -8,8 +8,62 @@ namespace Arkiv.Data
 {
     public interface ISqlData
     {
-        IEnumerable<T> SelectData<T>(string query, (string, object)[] parameters);
+        /// <summary>
+        /// Execute a query for selection of data from the db
+        /// </summary>
+        /// <typeparam name="T">The model which should be filled</typeparam>
+        /// <param name="query">The query which should be executed</param>
+        /// <param name="parameters">the parameter and it's corresponding value</param>
+        /// <remarks>As a web page it's best to do this action async</remarks>
+        /// <see  cref="SelectDataAsync{T}(string, (string, object)[])"/>
+        /// <returns>A set of all rows found in the db</returns>
+        IEnumerable<T> SelectData<T>(string query, (string, object)[] parameters = null);
 
-        Task<IEnumerable<T>> SelectDataAsync<T>(string query, (string, object)[] parameters);
+        /// <summary>
+        /// Execute a query for selection of data from the db
+        /// </summary>
+        /// <typeparam name="T">The model which should be filled</typeparam>
+        /// <param name="query">The query which should be executed</param>
+        /// <param name="parameters">the parameter and it's corresponding value</param>
+        /// <returns>A set of all rows found in the db</returns>
+        Task<IEnumerable<T>> SelectDataAsync<T>(string query, (string, object)[] parameters = null);
+
+        /// <summary>
+        /// Executes the given query with it's paramters attached
+        /// </summary>
+        /// <param name="query">the query to execute</param>
+        /// <param name="parameters">the parameters to attach</param>
+        /// <remarks>as a web page it's best to this action async</remarks>
+        /// <see cref="ExecuteAsync(string, (string, object)[])"/>
+        /// <returns>number of effected rows</returns>
+        int Execute(string query, (string, object)[] parameters = null);
+
+        /// <summary>
+        /// Executes the given query with it's paramters attached
+        /// </summary>
+        /// <param name="query">the query to execute</param>
+        /// <param name="parameters">the parameters to attach</param>
+        /// <returns>number of effected rows</returns>
+        Task<int> ExecuteAsync(string query, (string, object)[] parameters = null);
+
+        /// <summary>
+        /// Writes a log entry in the activity table
+        /// </summary>
+        /// <param name="action">What the user did</param>
+        /// <param name="time">when the user did it</param>
+        /// <param name="user">What user did it</param>
+        /// <param name="parameters">any additional data</param>
+        /// <remarks>as a web page it's best to do this action async</remarks>
+        /// <see cref="LogAsync(string, DateTime, string, string)"/>
+        void Log(string action, DateTime time, string user, string parameters);
+
+        /// <summary>
+        /// Writes a log entry in the activity table
+        /// </summary>
+        /// <param name="action">What the user did</param>
+        /// <param name="time">when the user did it</param>
+        /// <param name="user">What user did it</param>
+        /// <param name="parameters">any additional data</param>
+        void LogAsync(string action, DateTime time, string user, string parameters);
     }
 }
