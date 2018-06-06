fd254b0c549add3f4434fd0668d4864ae251acd4
e e <eDimbo4@Gmail.com>
2018-06-06 11:07:14 +0200

    Added some things

--- Arkiv/Controllers/ArchiveController.cs
+++ Arkiv/Controllers/ArchiveController.cs
@@ -4,26 +4,31 @@ using System.Collections.Generic;
 using System.Security.Principal;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Arkiv.Data.Filter;
 using Arkiv.Models;
 using System.Linq;
 using Arkiv.Data;
+using System;
 
 namespace Arkiv.Controllers
 {
     [Authorize]
     public class ArchiveController : Controller
     {
+        #region Constructor
         private ISqlData sql;
 
         public ArchiveController(ISqlData _data)
         {
             sql = _data;
         }
+        #endregion
 
+        #region Index
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            IEnumerable<ColumnNameModel> model = await sql.SelectDataAsync<ColumnNameModel>("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS", null);
+            IEnumerable<ColumnNameModel> model = await sql.SelectDataAsync<ColumnNameModel>("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS Where TABLE_NAME = 'arkiv'");
 
             List<SelectListItem> list = new List<SelectListItem>();
             
@@ -32,17 +37,96 @@ namespace Arkiv.Controllers
                 list.Add(new SelectListItem { Value = item.COLUMN_NAME, Text = item.COLUMN_NAME });
             }
 
-            //IEnumerable<ArchiveDataModel> data = await sql.SelectDataAsync<ArchiveDataModel>("SELECT * FROM arkiv", null);
+            IEnumerable<ArchiveDataModel> data = await sql.SelectDataAsync<ArchiveDataModel>("SELECT TOP 50 * FROM arkiv");
 
-            return View(new ArchiveJoinedModel() { selectListItems = list.AsEnumerable(), data = null });
+            return View(new ArchiveJoinedModel() { selectListItems = list.AsEnumerable(), data = data });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetTable(FilterModel[] Filters)
+        {
+            IEnumerable<ColumnNameModel> model = await sql.SelectDataAsync<ColumnNameModel>("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS Where TABLE_NAME = 'arkiv'");
+
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            //Add every column from the database to list
+            foreach (ColumnNameModel item in model)
+            {
+                list.Add(new SelectListItem { Value = item.COLUMN_NAME, Text = item.COLUMN_NAME });
+            }
+                
+            string WhereClause = "WHERE ";
+            List<(string, object)> ParamList = new List<(string, object)>();
+
+            //Make sure Filters is not empty
+            if (Filters.Count() > 0)
+            {
+                foreach(FilterModel Filter in Filters)
+                {
+                    //Determine which type, the current filter is
+                    switch(Filter.Type)
+                    {
+                        case "Single":
+                            //Check if the current item is the last item
+                            if(Filter.Equals(Filters[Filters.Length - 1]))
+                            {
+                                WhereClause += Filter.Name + " = " + "@" + Filter.Value.One.Replace(" ", string.Empty) + "";
+                                ParamList.Add(("@" + Filter.Value.One.Replace(" ", string.Empty), Filter.Value.One));
+                            }
+                            else
+                            {
+                                WhereClause += Filter.Name + " = " + "@" + Filter.Value.One.Replace(" ", string.Empty) + " AND ";
+                                ParamList.Add(("@" + Filter.Value.One.Replace(" ", string.Empty), Filter.Value.One));
+                            }
+                            break;
+                        case "Range":
+                            //Check if the current item is the last item
+                            if (Filter.Equals(Filters[Filters.Length - 1]))
+                            {
+                                WhereClause += Filter.Name + " BETWEEN " + "@" + Filter.Value.One.Replace(" ", string.Empty) + " AND " + "@" + Filter.Value.Two.Replace(" ", string.Empty) + "";
+                                ParamList.AddRange(new(string, object)[]
+                                {
+                                    ("@" + Filter.Value.One.Replace(" ", string.Empty), Filter.Value.One),
+                                    ("@" + Filter.Value.Two.Replace(" ", string.Empty), Filter.Value.Two)
+                                });
+                            }
+                            else
+                            {
+                                WhereClause += Filter.Name + " BETWEEN " + "@" + Filter.Value.One.Replace(" ", string.Empty) + " AND " + "@" + Filter.Value.Two.Replace(" ", string.Empty) + " AND ";
+                                ParamList.AddRange(new(string, object)[]
+                                {
+                                    ("@" + Filter.Value.One.Replace(" ", string.Empty), Filter.Value.One),
+                                    ("@" + Filter.Value.Two.Replace(" ", string.Empty), Filter.Value.Two)
+                                });
+                            }
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                WhereClause = string.Empty; //If Filters is empty, make WhereClause empty
+            }
+
+            IEnumerable<ArchiveDataModel> data = await sql.SelectDataAsync<ArchiveDataModel>("SELECT * FROM arkiv " + WhereClause, ParamList.ToArray());
+
+            //If there is no results, return the json object, "No Match"
+            if (data.Count() == 0)
+            {
+                return Json("No Match");
+            }
+
+            return PartialView("TablePartial",new ArchiveJoinedModel() { selectListItems = list.AsEnumerable(), data = data });
         }
 
         [HttpPost]
         public IActionResult GetFilterPartial(string SelectedColumn)
         {
             return PartialView("FilterPartial", SelectedColumn);
-        }   
+        }
+        #endregion
 
+        #region Test
         [HttpGet]
         public IActionResult Test()
         {
@@ -55,14 +139,16 @@ namespace Arkiv.Controllers
                 {
                     groups.Add(t.Translate(typeof(NTAccount)).Value);
                 }
-                catch (System.Exception)
+                catch (Exception)
                 {
                     groups.Add(t.Value);
                 }
 
             return Json(new { isInGroup = test.IsInRole("Administrators"), User.Identity.Name,  groups});
         }
+        #endregion
 
+        #region Admin
         [HttpGet]
         public IActionResult Admin()
         {
@@ -73,6 +159,6 @@ namespace Arkiv.Controllers
 
             return Redirect("Index");
         }
-
+        #endregion
     }
 }
---
+++ Arkiv/Data/Filter/FilterModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Arkiv.Data.Filter
+{
+    public class FilterModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public FilterValueModel Value { get; set; }
+    }
+
+    public class FilterValueModel
+    {
+        public string One { get; set; }
+        public string Two { get; set; }
+    }
+}
