49e87e8623d8e408a677ffba2edba9fe83de984b
Henrik Malmer Lassen <lasssenn@gmail.com>
2018-06-06 08:43:34 +0200

    Added Logging

--- Arkiv/Program.cs
+++ Arkiv/Program.cs
@@ -20,6 +20,12 @@ namespace Arkiv
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
